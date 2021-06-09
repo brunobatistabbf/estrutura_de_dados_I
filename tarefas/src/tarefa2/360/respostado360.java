c2e6ffdfce7bc03cb486270d75a2b77c953114c4
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2021-06-08 21:04:33 -0300

    work_signos

---
+++ tarefas/src/tarefa2/360/respostado360.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class respostado360 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);   
+    int dia, data,  mes;
+    int i = 0;
+    int[] ultdia = new int[12];
+    String[] signo = new String[12];    
+     
+    do {
+     System.out.println("Digite signo");
+        signo[i] = sc.next();
+     System.out.println("Digite ultimo dia: ");
+        ultdia[i] = sc.nextInt();
+     i++;  
+    } while (i < 12);
+    System.out.println("Digite data no formato ddmm ou 9999 para terminar: ");
+     data =  sc.nextInt();
+
+    do {
+        dia = data/100;
+        mes = data % 100;
+        mes--;
+
+        if (dia > ultdia[mes]) {
+            mes = (mes + 1) % 12;
+        }
+            System.out.println("Signos : " + signo[mes] + "\n");
+            System.out.println("Digite data formato ddmm ou 9999 para terminar: ");
+            data = sc.nextInt();
+            System.out.println();   
+    } while (data != 9999);
+
+    }
+}
---
+++ tarefas/src/tarefa2/360/respostafor360.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class respostafor360 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dia, i, data,  mes;
+        int[] ultdia = new int[12];
+        String[] signo = new String[12];
+
+        for ( i = 0; i < 12; i++) {
+            System.out.println("Digite signo");
+            signo[i] = sc.next();
+            System.out.println("Digite ultimo dia: ");
+            ultdia[i] = sc.nextInt();
+        }
+        System.out.println("Digite data no formato ddmm ou 9999 para terminar: ");
+        data =  sc.nextInt();
+
+        for (; data != 9999; ) {
+            dia = data/100;
+            mes = data % 100;
+            mes--;
+
+            if (dia > ultdia[mes]) {
+                mes = (mes + 1) % 12;
+            }
+            System.out.println("Signos : " + signo[mes] + "\n");
+            System.out.println("Digite data formato ddmm ou 9999 para terminar: ");
+            data = sc.nextInt();
+
+        }
+
+
+    }
+}
