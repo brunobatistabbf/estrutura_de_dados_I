fb2929c28fcf0d56e682a54f6a24d7caf92712f8
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2021-06-14 22:12:40 -0300

    work_428

---
+++ tarefas/src/tarefa2/428/respostafor428.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class respostafor428 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] dados = new int[10][4];
+        int l ;
+        int codigo;
+        int pos;
+        int aux;
+        for (l = 0; l < 10; l++) {
+            System.out.println("Digite a matricula no formato aascccnnn: ");
+            dados[l][0] = sc.nextInt();
+            aux = dados[l][0]/1000000;
+            aux = dados[l][0] - aux * 1000000;
+            dados[l][2] = aux/1000;
+            System.out.println("\ndigite sexo (0 fem 1 masc): ");
+            dados[l][1] = sc.nextInt();
+            System.out.println("\ndigite CR do aluno: ");
+            dados[l][3] = sc.nextInt();
+
+        }
+        System.out.println("Digite o codigo do curso: ");
+        codigo = sc.nextInt();
+        for ( l = 0; l < 10; l++) {
+            if (dados[L][2]==codigo && dados[L][1]==0) {
+                pos = l;
+            } else {
+                if (dados [L] [3] >dados [pos][3]) {
+                    pos = l;
+                }
+            }
+        }
+    
+
+
+
+        System.out.println("\nAluna premiada matricula: " + dados[pos][O] + " teve CR:" +dados[pos][3]);
+        System.out.println();
+    }
+}
